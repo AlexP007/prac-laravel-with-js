fd873ebbaad5422d647041ddf95edecc0175e64f
sasha <asqed@mail.ru>
2021-12-01 21:27:36 +0300

    [i] update done

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Http\Requests\CarPostStoreRequest;
+use App\Http\Requests\UserPostGeneratedRequest;
 
 class UsersController extends Controller
 {
@@ -13,23 +13,96 @@ class UsersController extends Controller
         return 'Страница регистрации';
     }
 
-    public function generatedUser(CarPostStoreRequest $request)
+    public function register(UserPostGeneratedRequest $request)
     {
 
         $validate = $request->validated();
         $user = new User($validate);
 
+        $token = $user->generateAndSaveToken();
+
         if($user->save()){
-            return redirect()
-                ->route('profile')
-                ->createToken($request->token_name);
+            
+            $data = [
+                'data' => [
+                    'api_token' => $token,
+                    'email' =>  $user->email,
+                    'id' =>  $user->id,
+                    'name' =>  $user->name,
+                    'surname' =>  $user->surname,
+                ]
+            ];
+
+            return response($data, 201);
         }
          
         
     }
 
-    public function index()
+    public function login(Request $request)
     {
-        return 'Здесь страница пользователя';
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = User::where('email', $email)->first();
+
+        if($user->password === $password){
+            $token = $user->generateAndSaveToken();
+
+                $data = [
+                    'data' => [
+                        'api_token' => $token,
+                        'email' => $user->email,
+                        'id' => $user->id,
+                        'name' => $user->name,
+                        'surname' => $user->surname,
+                    ]
+                ];
+    
+                return response($data, 201);
+        }
     }
+
+    public function update(UserPostGeneratedRequest $request)
+    {
+        
+        $token = $request->bearerToken();
+        $user = User::where('remember_token', $token)->first();
+
+        $error = [
+            'eroros' => [
+                'Authorized' => ['Not authorized']
+            ],
+        ];
+
+        if (!$user) {
+            return response($error, 401);
+        }
+
+        $validate = $request->validated();
+
+        // dd($validate);
+
+        $user->name = $validate['name'];
+        $user->password = $validate['password'];
+        $user->surname = $validate['surname']; 
+
+        $token = $user->generateAndSaveToken();
+
+        $data = [
+            'data' => [
+                'api_token' => $token,
+                'email' => $user->email,
+                'id' => $user->id,
+                'name' => $user->name,
+                'surname' => $user->surname,
+            ]
+        ];
+
+        // проверяем есть ли такой токен, если есть все хорошо, если нет то вернули 401 ошибку        
+        
+        return response($error, 401);
+    }
+
 }
+
